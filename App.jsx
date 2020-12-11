c846a6ec5ae4c1d99cb6118ee4183b73ab08fc5d
Sawan Khanchi <sappler16@gmail.com>
2020-12-11 19:55:13 +0530

    update commit

--- App.jsx
+++ App.jsx
@@ -23,29 +23,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
-/* React Hooks Challenge*/
-
-import React, { useState } from 'react';
-
-const App = () => {
-  let newTime = new Date().toLocaleTimeString();
-  const [cTime, setCtime] = useState(newTime);
-
-  const UpdateTime = () => {
-    newTime = new Date().toLocaleTimeString();
-    setCtime(newTime);
-  };
-  return (
-    <>
-    <h1> {cTime} </h1>
-    <button onClick={UpdateTime}> Get Time </button>
-    </>
-
-  );
-}
-
-export default App;

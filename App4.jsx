be18bc5cac458ac2dbe7b87df8299a41b6786bc3
Sawan Khanchi <sappler16@gmail.com>
2020-12-15 14:20:54 +0530

    update commit

---
+++ App4.jsx
@@ -0,0 +1,56 @@
+/* React Login Forms */
+
+import React, { useState } from 'react';
+
+const App = () => {
+  const [name, setName] = useState("");
+  
+  const [lastName, setFullName] = useState("");
+
+  const [fullName, setFullName] = useState("");
+
+  const [lastNamenew, setLastNamenew] = useState("");
+
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    setName(event.target.value);
+  };
+
+  const inputEventTwo = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+      event.preventDefault();
+      setFullName(name);
+      setLastNamenew(lastName);
+  };
+
+  return (
+    <>
+      <div className="main_div">
+        <form onSubmit={onSubmit}>
+          <div>
+            <h1> Hello {fullName} {lastNamenew} </h1>
+            <input 
+              type='text' 
+              placeholder="Enter Your Name" 
+              onChange={inputEvent}
+              value={name} 
+            />
+            <br />
+            <input 
+              type='text' 
+              placeholder="Enter Your Last Name" 
+              onChange={inputEventTwo}
+              value={lastName} 
+             />
+            <button type="submit"> Sumbit Me 👍 </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default App;

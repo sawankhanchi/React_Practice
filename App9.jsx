bb392ccbfcca65c343ae9c54d679352e995ab24f
Sawan Khanchi <sappler16@gmail.com>
2020-12-26 16:34:58 +0530

    react useEffect

---
+++ App9.jsx
@@ -0,0 +1,37 @@
+/* React useEffect */
+
+import React, { useState, useContext, useEffect } from "react";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [nums, setNums] = useState(0);
+
+  useEffect(() => {
+    alert("I am clicked");
+    console.log("I am clicked");
+  }, [num]);
+
+  return (
+    <>
+    <button 
+      onClick={() => {
+        setNum(num + 1);
+      }
+     } 
+    > 
+      Click me {num} 
+    </button>
+    <br />
+    <button 
+      onClick={() => {
+        setNums(nums + 1);
+      }
+     } 
+    > 
+      Click me {nums} 
+    </button>
+    </>
+  )
+};
+
+export default App;

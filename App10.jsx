464b10490098f1936364213ac7eed255a151e25f
Sawan Khanchi <sappler16@gmail.com>
2020-12-29 13:29:05 +0530

    react router dom

---
+++ App10.jsx
@@ -0,0 +1,32 @@
+/* React Router DOM */
+
+import React from "react";
+import Menu from "./Menu";
+import About from "./About";
+import Service from "./Service";
+import Contact from "./Contact";
+import Error from "./Error";
+
+const App = () => {
+    const Name = () => {
+        return <h1> Hello, I am a name page </h1>
+    };
+
+    return (
+        <>
+        <Menu />
+        <Switch>
+            <Route 
+              exact 
+              path="/" 
+              component={() => <About name="About" />} />
+            <Route 
+              exact 
+              path="/service" 
+              render={() => <Service name="Service" />} />
+            <Route path="/contact" component={Contact} />
+            <Route component={Error} />
+        </Switch>
+        </>
+    );
+};
